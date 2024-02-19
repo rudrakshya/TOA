f02e6ed93ee66ad153b3f731b509f898c265e160
Rudrakshya Barman <rudrakshya@adytumtech.com>
2024-02-19 10:57:56 +0530

    make the qr scanner area smaller to scan from a distance.

--- lib/modules/scanner/qr_scanner.dart
+++ lib/modules/scanner/qr_scanner.dart
@@ -104,8 +104,8 @@ class _QRScannerState extends State<QRScanner> {
     // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
     var scanArea = (MediaQuery.of(context).size.width < 400 ||
             MediaQuery.of(context).size.height < 400)
-        ? 250.0
-        : 300.0;
+        ? 150.0
+        : 250.0;
     // To ensure the Scanner view is properly sizes after rotation
     // we need to listen for Flutter SizeChanged notification and update controller
     return QRView(
